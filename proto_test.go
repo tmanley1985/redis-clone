11a2984dcabbf624026487bc086c320a11eda356
Thomas Manley <tmanley1985@gmail.com>
2024-06-27 08:34:13 -0400

    Getting basic test not failing

--- proto_test.go
+++ proto_test.go
@@ -1,9 +1,32 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"testing"
+
+	"github.com/tidwall/resp"
+)
 
 func TestProtocol(t *testing.T) {
-	msg := "*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$3\r\nbar\r\n"
-
-	parseCommand(msg)
+	raw := "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
+	rd := resp.NewReader(bytes.NewBufferString(raw))
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Read %s\n", v.Type())
+		if v.Type() == resp.Array {
+			for i, v := range v.Array() {
+				fmt.Printf("  #%d %s, value: '%s'\n", i, v.Type(), v)
+			}
+		}
+	}
+	parseCommand(raw)
 }
