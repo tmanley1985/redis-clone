74f25797703fc23cd997df9e64fa29dacad96750
Thomas Manley <tmanley1985@gmail.com>
2024-06-27 08:27:29 -0400

    Deciding to build server first, then get rid of dependency on protocol

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module redis-clone
 
 go 1.22.2
+
+require github.com/tidwall/resp v0.1.1 // indirect
--- proto.go
+++ proto.go
@@ -1,9 +1,18 @@
 package main
 
-type Command struct {
+import "fmt"
 
+type Command struct {
 }
 
-func parseCommand(msg []byte) (Command, error) {
+func parseCommand(msg string) (Command, error) {
+	t := msg[0]
+
+	fmt.Println(t)
+
+	switch t {
+	case '*':
+		fmt.Println(msg[1:])
+	}
 	return Command{}, nil
 }
